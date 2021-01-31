f6a58dbe4208468f4d2e31f6ffb496c0f80a4672
Sarah Dreischer <sarah.dreischer@gmail.com>
2021-01-31 00:56:46 +0000

    Added different sized favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,24 @@ class MyDocument extends Document {
             `,
             }}
           />
+          <link
+            rel="icon"
+            href="/favicons/favicon-48x48.ico"
+            sizes="48x48"
+            type="image/x-icon"
+          />
+          <link
+            rel="icon"
+            href="/favicons/favicon-32x32.ico"
+            sizes="32x32"
+            type="image/x-icon"
+          />
+          <link
+            rel="icon"
+            href="/favicons/favicon-16x16.ico"
+            sizes="16x16"
+            type="image/x-icon"
+          />
           <meta content="68063a32bf98d316" name="yandex-verification" />
         </Head>
         <body>
