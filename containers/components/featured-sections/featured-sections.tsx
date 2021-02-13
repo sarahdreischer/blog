794c03fa600abf063b2153fed128081b8db2ecc2
Sarah Dreischer <sarah.dreischer@gmail.com>
2021-02-13 17:42:18 +0000

    Updated featured sections style

--- containers/components/featured-sections/featured-sections.tsx
+++ containers/components/featured-sections/featured-sections.tsx
@@ -5,34 +5,25 @@ import styles from "./featured-sections.module.css";
 interface FeaturedProps {}
 
 export const FeaturedSections = (props: FeaturedProps) => {
-  const onButtonClick = () => console.log("Clicked Read more");
-
   return (
     <Row className="p-3 h-100">
       <Col className={`${styles.largeImage} pl-0`} md={8}>
         <OverlayImage
-          url="/featured-sections/image-1.jpeg"
+          imageUrl="/featured-sections/image-1.jpeg"
           title="Create a blog with Next.JS"
-          buttonText="Posts coming soon"
-          onButtonClick={onButtonClick}
         />
       </Col>
       <Col md={4}>
         <Row className={`mb-2 ${styles.smallImage}`}>
           <OverlayImage
-            url="/featured-sections/image-2.jpeg"
+            imageUrl="/featured-sections/image-2.jpeg"
             title="Tutorials"
-            buttonText="Posts coming soon"
-            onButtonClick={onButtonClick}
-            invertedOverlay
           />
         </Row>
         <Row className={`mt-2 ${styles.smallImage}`}>
           <OverlayImage
-            url="/featured-sections/image-3.jpeg"
+            imageUrl="/featured-sections/image-3.jpeg"
             title="Projects"
-            buttonText="Posts coming soon"
-            onButtonClick={onButtonClick}
           />
         </Row>
       </Col>
--- containers/components/overlay-image/overlay-image.tsx
+++ containers/components/overlay-image/overlay-image.tsx
@@ -1,41 +1,26 @@
 import Image from "next/image";
-import { Container, Button, Row } from "react-bootstrap";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { Container, Row } from "react-bootstrap";
 import styles from "./overlay-image.module.css";
 
 interface OverlayImageProps {
   title: string;
-  url: string;
-  buttonText: string;
-  onButtonClick: () => void;
-  invertedOverlay?: boolean;
+  imageUrl: string;
 }
 
-export const OverlayImage = ({
-  title,
-  url,
-  buttonText,
-  onButtonClick,
-  invertedOverlay,
-}: OverlayImageProps) => {
+export const OverlayImage = ({ title, imageUrl: url }: OverlayImageProps) => {
   return (
     <div className={styles.imageContainer}>
       <Image src={url} alt={title} objectFit="cover" layout="fill" priority />
-      <div
-        className={invertedOverlay ? styles.invertedOverlay : styles.overlay}
-      >
-        <Container className="h-75">
-          <Row className="h-100 justify-content-center align-items-center">
-            <div className={`text-white ${styles.overlayTextSize}`}>
-              {title}
-            </div>
-          </Row>
-          <Row className="h-25 justify-content-center align-items-center">
-            <Button variant="outline-light" onClick={onButtonClick} disabled>
-              {buttonText}
-            </Button>
-          </Row>
-        </Container>
-      </div>
+      <Container className="h-100 justify-content-center align-items-center">
+        <Row className="h-100 justify-content-center align-items-center">
+          <button
+            className={`d-flex align-items-center text-white px-1 ${styles.imageTitle}`}
+          >
+            {title} <ArrowForwardIosIcon className={`${styles.arrow}`} />
+          </button>
+        </Row>
+      </Container>
     </div>
   );
 };
