98bc375b7151158bf5babf9b6ff6fd1e83ae7af5
Sarah Dreischer <sarah.dreischer@gmail.com>
2021-11-27 18:47:29 +0000

    Finished header component

--- components/Layout/components/Header/Header.tsx
+++ components/Layout/components/Header/Header.tsx
@@ -1,12 +1,14 @@
-// import InstagramIcon from '@mui/icons-material/Instagram';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { Toolbar } from '@mui/material';
 import cn from 'classnames';
-import React from 'react';
+import Link from 'next/link';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
-    <Toolbar className='d-flex justify-content-between w-100 pt-1'>
+    <Toolbar className='d-flex justify-content-between w-100 pt-2'>
       <div className={cn('h4', styles.logo)}>
         <strong>
           Software
@@ -16,7 +18,29 @@ export default function Header() {
           Sarah
         </strong>
       </div>
-      <div>{/* <InstagramIcon /> */}</div>
+      <div className={cn('d-flex', styles.socialMedia)}>
+        <div className={styles.link}>
+          <Link href='https://www.instagram.com/softwarewithsarah/' passHref={true}>
+            <a href='' target='_blank' rel='nofollow'>
+              <InstagramIcon />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href='https://www.linkedin.com/in/sarah-dreischer-2878a4107' passHref={true}>
+            <a href='' target='_blank' rel='nofollow'>
+              <LinkedInIcon />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.link}>
+          <Link href='https://github.com/sarahdreischer' passHref={true}>
+            <a href='' target='_blank' rel='nofollow'>
+              <GitHubIcon />
+            </a>
+          </Link>
+        </div>
+      </div>
     </Toolbar>
   );
 }
--- components/SocialMedia/ShareButtons.tsx
+++ components/SocialMedia/ShareButtons.tsx
@@ -2,10 +2,9 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import PinterestIcon from '@mui/icons-material/Pinterest';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Link from 'next/link';
 import { useRouter } from 'next/dist/client/router';
-import styles from './ShareButtons.module.css';
-import React from 'react';
+import Link from 'next/link';
+import styles from './ShareButtons.module.scss';
 
 interface ShareButtonsProps {
   title: string;
